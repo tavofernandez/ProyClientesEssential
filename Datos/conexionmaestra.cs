9a0c3f94f40e9aa1714ea2a887df80b814f59023
Gustavo Fernandez <tavovicious@gmail.com>
2024-10-22 23:43:40 -0500

    Creamos dentro de la capa de Datos una clase para establecer la conexion a la base de datos

---
+++ Datos/conexionmaestra.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ProyClientesEssential.Datos
+{
+    public class conexionmaestra
+    {
+        //autentificacion de windows
+        public static string conexion = @"Data source=DESKTOP-4N38VGD\SQLEXPRESS; Inital Catalog=ClientesEssentialDB; Integrated Security=true";
+        
+        public static SqlConnection conectar = new SqlConnection(conexion);
+
+        public static void abrir()
+        {
+            if (conectar.State == ConnectionState.Closed)
+            {
+                conectar.Open();
+            }
+        }
+
+        public static void cerrar()
+        {
+            if (conectar.State == ConnectionState.Open)
+            {
+                conectar.Close();
+            }
+        }
+    }
+}
