e1c10bbf4f2b18bfbc5428ac72d4dc3fdff2978a
Gustavo Fernandez <tavovicious@gmail.com>
2024-10-22 22:51:00 -0500

    Dentro de la capa logica creamos una clase que llevara los datos de los clientes que manejara la aplicacion

---
+++ Logica/Lclientes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProyClientesEssential.Logica
+{
+    public class Lclientes
+    {
+        public int Codigo { get; set; }
+
+        public string Nombre { get; set; }
+
+        public int Edad { get; set; }
+
+
+    }
+}
