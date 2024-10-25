1d981e524e7ac89515641422461d524562320184
Gustavo Fernandez <tavovicious@gmail.com>
2024-10-24 22:18:12 -0500

    Dentro de la capa presentacion creamos una carpeta que contendra el formulario en donde diseñamos la interfaz de usuario

---
+++ Presentacion/Crud/frmCrud.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProyClientesEssential.Presentacion.Crud
+{
+    public partial class frmCrud : Form
+    {
+        public frmCrud()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmCrud_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
